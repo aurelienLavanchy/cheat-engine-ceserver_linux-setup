2fbd0c6847858b209f7f97dc02a703fdd0f30ec5
Aurélien Lavanchy <43612210+akeryuuth@users.noreply.github.com>
2023-05-24 10:22:08 +0200

    Update CE-install.sh

--- CE-install.sh
+++ CE-install.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/sh
+
 echo "Enter the desired path for Cheat Engine (that doesn't require root privilege because I'm bad at writing scripts)"
 read install_path
 version = curl -Is https://github.com/cheat-engine/cheat-engine/releases/latest | awk -F '/' '/^location/ {print substr($NF, 1, length($NF-1))}'    # source: https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8?permalink_comment_id=4411890#gistcomment-4411890
@@ -11,8 +12,10 @@ cd 'cheat-engine-$version/Cheat Engine/ceserver/gcc'
 make
 mv ceserver $install_path
 
-cd ../../../../..
+cd -
 rm -rf cheat-engine-$version && rm cheat-engine-$version.zip
 
 wget https://github.com/cheat-engine/cheat-engine/releases/download/$version/CheatEngine$version.exe
 # install Cheat Engine via Lutris
+
+#wrap-up installation
