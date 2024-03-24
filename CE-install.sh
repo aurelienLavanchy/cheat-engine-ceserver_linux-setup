51fbfdc2ab063a5080b8b6cb3d0368a8ded7539f
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-03-24 14:39:43 +0100

    feat: fleshed out error messages & remove wget dependency

--- CE-install.sh
+++ CE-install.sh
@@ -1,32 +1,38 @@
-#! /usr/bin/sh
+#! /usr/bin/bash
 
-# initial setup
+# variables
 version=$(curl -Is https://github.com/cheat-engine/cheat-engine/releases/latest | awk -F '/' '/^location/ {print substr($NF, 1, length($NF-1))}')    # source: https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8?permalink_comment_id=4411890#gistcomment-4411890
 dl_version=$(echo "$version" | sed -e 's/\.//g')
-temp_path=$HOME/Documents/cheat-engine-temp
-# add a check to remove existing $temp_path
-mkdir "$temp_path"
+# bin_install_path="/usr/bin"
+temp_path=$HOME/cheat-engine-temp
+if [ -d "$temp_path" ]; then
+  mkdir "$temp_path"
+fi
 
-# grabbing the source code archive from the latest realease
-cd "$temp_path" || exit
-wget "https://github.com/cheat-engine/cheat-engine/archive/refs/tags/$version.zip" -O "cheat-engine-$version.zip"
+## Installation
+# 1. Grabbing the source code archive from the latest release
+cd "$temp_path" || error "$temp_path does not exist or was not found" && exit 1
+curl "https://github.com/cheat-engine/cheat-engine/archive/refs/tags/$version.zip" -O "cheat-engine-$version.zip"
 unzip "cheat-engine-$version.zip"
 
-# building ceserver and installing it
-cd "cheat-engine-$version/Cheat Engine/ceserver/gcc" || exit
+# 2. Building ceserver and 'installing' it
+cd "cheat-engine-$version/Cheat Engine/ceserver/gcc" || error "Was the release archive decompressed properly?" && exit 2
 make --quiet > /dev/null 2>&1    # silence make errors and warnings
 mkdir "$HOME/Cheat-Engine"
-mv ceserver ~/Cheat-Engine || exit
+mv ceserver ~/Cheat-Engine || error "ceserver was not found, did make correctly build Cheat Engine ?" && exit 3
+# echo "ceserver resides in $HOME/Cheat-Engine do you want to make it available globally ? (create a symlink to $bin_install_path)"
 
 # cleaning up
-cd - || exit
+cd - || exit 9
 rm -rf "cheat-engine-$version" && rm "cheat-engine-$version.zip"
 
 # installing cheat engine
-wget "https://github.com/cheat-engine/cheat-engine/releases/download/$version/CheatEngine$dl_version.exe"
+curl "https://github.com/cheat-engine/cheat-engine/releases/download/$version/CheatEngine$dl_version.exe"
 WINEPREFIX="$HOME/Cheat-Engine" wine "$HOME/Documents/cheat-engine-temp/CheatEngine$dl_version.exe"
 
 # wrapping-up the installation
-cd "$HOME/Documents" || exit
+cd "$HOME" || exit 9
 rm -rf "cheat-engine-temp"
-printf "\n" && echo "Done!"
+printf "\n" && echo "Done! Don't forget to launch ceserver before trying to connect to it in Cheat Engine"
+
+exit 0
--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 **If not already installed**
 
-- wget
 - curl
 - make
 - unzip
@@ -13,17 +12,14 @@
 ## Usage
 
 1. Allow script to be executable by either
-  Typing `chmod u+x CE-install.sh`  
-  OR right-clicking on the script and selecting Properties > Permissions then ticking "[ ] Is executable"
+   Typing `chmod u+x CE-install.sh`
+   OR right-clicking on the script and selecting Properties > Permissions then ticking "[ ] Is executable"
 2. Launch script via terminal
-  In the directory the script is located: `./CE-install.sh`,  
-  proceed with the rest of the installation
+   In the directory the script is located: `./CE-install.sh`,
+   proceed with the rest of the installation
 
 ## Notes / TODO
 
-- **No error handling**
-- This script downloads the source code archive in Cheat Engine's releases to build ceserver instead of cloning the repo, this is intended as ceserver's "version" must match Cheat Engine's in order to work properly (need confirmation)  
-- invoke something other than `/usr/bin/sh`
-- installs everything in $HOME/Cheat-Engine (for now)
-
-Made with VSCode + ShellCheck extension
+- Proper error handling
+- This script downloads the source code archive in Cheat Engine's releases to build ceserver instead of cloning the repo, this is intended as ceserver's "version" must match Cheat Engine's in order to work properly (need confirmation)
+- no user-defined install path => installs everything in $HOME/Cheat-Engine (for now)
